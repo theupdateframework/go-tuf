0f690ae3c914c0b5d293c14a6bf79f5128314e76
Lewis Marshall <lewis@lmars.net>
2015-01-08 15:15:21 +0000

    signed: Don't add duplicate signatures
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- signed/sign.go
+++ signed/sign.go
@@ -7,11 +7,19 @@ import (
 )
 
 func Sign(s *data.Signed, k *data.Key) {
+	id := k.ID()
+	signatures := make([]data.Signature, 0, len(s.Signatures)+1)
+	for _, sig := range s.Signatures {
+		if sig.KeyID == id {
+			continue
+		}
+		signatures = append(signatures, sig)
+	}
 	priv := [ed25519.PrivateKeySize]byte{}
 	copy(priv[:], k.Value.Private)
 	sig := ed25519.Sign(&priv, s.Signed)
-	s.Signatures = append(s.Signatures, data.Signature{
-		KeyID:     k.ID(),
+	s.Signatures = append(signatures, data.Signature{
+		KeyID:     id,
 		Method:    "ed25519",
 		Signature: sig[:],
 	})
