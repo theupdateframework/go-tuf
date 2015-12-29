9e56ce7d10cc6acb8673acb311ff0b44d860d1dd
Grant Rodgers <grantr@gmail.com>
2015-12-29 15:46:01 -0800

    remove url method
    S3 client handles leading slash or not.

--- client/s3_remote_store.go
+++ client/s3_remote_store.go
@@ -48,27 +48,18 @@ func (h *s3RemoteStore) GetTarget(name string) (io.ReadCloser, int64, error) {
 }
 
 func (h *s3RemoteStore) get(s string) (io.ReadCloser, int64, error) {
-	u := h.url(s)
-
 	svc := s3.New(h.session)
 	res, err := svc.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String("bucket"),
-		Key:    aws.String(u),
+		Key:    aws.String(s),
 	})
 	if err != nil {
 		return nil, 0, &url.Error{
 			Op:  "GET",
-			URL: u,
+			URL: s,
 			Err: err,
 		}
 	}
 
 	return res.Body, *res.ContentLength, nil
-}
-
-func (h *s3RemoteStore) url(path string) string {
-	if !strings.HasPrefix(path, "/") {
-		path = "/" + path
-	}
-	return h.baseURL + path
 }
