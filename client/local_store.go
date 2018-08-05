9de12d7af06f6cb229d32cddcd6abb96950aba0e
James Tucker <raggi@google.com>
2018-08-05 13:11:41 -0700

    [local_store] make the leveldb store a package
    We don't want to compile in leveldb and snappy in some cases, so this is
    moved to it's own package. We can likely remove it later.
    
    Change-Id: I6415ec8667d6f704ae28fa3132d543885f179f2c

--- client/local_store.go
+++ client/local_store.go
@@ -2,9 +2,6 @@ package client
 
 import (
 	"encoding/json"
-
-	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/storage"
 )
 
 func MemoryLocalStore() LocalStore {
@@ -20,34 +17,4 @@ func (m memoryLocalStore) GetMeta() (map[string]json.RawMessage, error) {
 func (m memoryLocalStore) SetMeta(name string, meta json.RawMessage) error {
 	m[name] = meta
 	return nil
-}
-
-func FileLocalStore(path string) (LocalStore, error) {
-	fd, err := storage.OpenFile(path, false)
-	if err != nil {
-		return nil, err
-	}
-
-	db, err := leveldb.Open(fd, nil)
-	return &fileLocalStore{db: db}, err
-}
-
-type fileLocalStore struct {
-	db *leveldb.DB
-}
-
-func (f *fileLocalStore) GetMeta() (map[string]json.RawMessage, error) {
-	meta := make(map[string]json.RawMessage)
-	db_itr := f.db.NewIterator(nil, nil)
-	for db_itr.Next() {
-		vcopy := make([]byte, len(db_itr.Value()))
-		copy(vcopy, db_itr.Value())
-		meta[string(db_itr.Key())] = vcopy
-	}
-	db_itr.Release()
-	return meta, db_itr.Error()
-}
-
-func (f *fileLocalStore) SetMeta(name string, meta json.RawMessage) error {
-	return f.db.Put([]byte(name), []byte(meta), nil)
 }
