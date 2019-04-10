1c3154eb531d7768ebbd17988b8c0eaa0dcafa95
Erick Tryzelaar <etryzelaar@google.com>
2019-04-09 21:22:31 -0700

    Remove support for leading path separators in targets
    TUF 1.0 is considering banning leading path separators, because
    of poor behavior constructing paths across targets and delegates
    on local filesystems for certain libraries, like python, where
    `os.path.join("/foo", "/bar")` just returns "/bar". This patch
    migrates go-tuf to using and generating metadata without that
    leading separator. However, it temporarily enables consuming
    metadata with separators to enable a rolling upgrade to 1.0.
    
    Change-Id: Ib7331ecb560fa85098fab6aef8815154905b79f1

--- client/client_test.go
+++ client/client_test.go
@@ -81,9 +81,9 @@ func (f *fakeFile) Close() error {
 }
 
 var targetFiles = map[string][]byte{
-	"/foo.txt": []byte("foo"),
-	"/bar.txt": []byte("bar"),
-	"/baz.txt": []byte("baz"),
+	"foo.txt": []byte("foo"),
+	"bar.txt": []byte("bar"),
+	"baz.txt": []byte("baz"),
 }
 
 func (s *ClientSuite) SetUpTest(c *C) {
@@ -275,7 +275,7 @@ func (s *ClientSuite) TestFirstUpdate(c *C) {
 	files, err := s.newClient(c).Update()
 	c.Assert(err, IsNil)
 	c.Assert(files, HasLen, 1)
-	assertFiles(c, files, []string{"/foo.txt"})
+	assertFiles(c, files, []string{"foo.txt"})
 }
 
 func (s *ClientSuite) TestMissingRemoteMetadata(c *C) {
@@ -361,14 +361,14 @@ func (s *ClientSuite) TestNewTargets(c *C) {
 	client := s.newClient(c)
 	files, err := client.Update()
 	c.Assert(err, IsNil)
-	assertFiles(c, files, []string{"/foo.txt"})
+	assertFiles(c, files, []string{"foo.txt"})
 
 	s.addRemoteTarget(c, "bar.txt")
 	s.addRemoteTarget(c, "baz.txt")
 
 	files, err = client.Update()
 	c.Assert(err, IsNil)
-	assertFiles(c, files, []string{"/bar.txt", "/baz.txt"})
+	assertFiles(c, files, []string{"bar.txt", "baz.txt"})
 
 	// Adding the same exact file should not lead to an update
 	s.addRemoteTarget(c, "bar.txt")
@@ -772,7 +772,7 @@ func (s *ClientSuite) TestUpdateHTTP(c *C) {
 		// check update is ok
 		targets, err := client.Update()
 		c.Assert(err, IsNil)
-		assertFiles(c, targets, []string{"/foo.txt", "/bar.txt", "/baz.txt"})
+		assertFiles(c, targets, []string{"foo.txt", "bar.txt", "baz.txt"})
 
 		// check can download files
 		for name, data := range targetFiles {
@@ -797,15 +797,15 @@ func (t *testDestination) Delete() error {
 func (s *ClientSuite) TestDownloadUnknownTarget(c *C) {
 	client := s.updatedClient(c)
 	var dest testDestination
-	c.Assert(client.Download("/nonexistent", &dest), Equals, ErrUnknownTarget{"/nonexistent"})
+	c.Assert(client.Download("nonexistent", &dest), Equals, ErrUnknownTarget{"nonexistent"})
 	c.Assert(dest.deleted, Equals, true)
 }
 
 func (s *ClientSuite) TestDownloadNoExist(c *C) {
 	client := s.updatedClient(c)
-	delete(s.remote.targets, "/foo.txt")
+	delete(s.remote.targets, "foo.txt")
 	var dest testDestination
-	c.Assert(client.Download("/foo.txt", &dest), Equals, ErrNotFound{"/foo.txt"})
+	c.Assert(client.Download("foo.txt", &dest), Equals, ErrNotFound{"foo.txt"})
 	c.Assert(dest.deleted, Equals, true)
 }
 
@@ -823,19 +823,19 @@ func (s *ClientSuite) TestDownloadOK(c *C) {
 func (s *ClientSuite) TestDownloadWrongSize(c *C) {
 	client := s.updatedClient(c)
 	remoteFile := &fakeFile{buf: bytes.NewReader([]byte("wrong-size")), size: 10}
-	s.remote.targets["/foo.txt"] = remoteFile
+	s.remote.targets["foo.txt"] = remoteFile
 	var dest testDestination
-	c.Assert(client.Download("/foo.txt", &dest), DeepEquals, ErrWrongSize{"/foo.txt", 10, 3})
+	c.Assert(client.Download("foo.txt", &dest), DeepEquals, ErrWrongSize{"foo.txt", 10, 3})
 	c.Assert(remoteFile.bytesRead, Equals, 0)
 	c.Assert(dest.deleted, Equals, true)
 }
 
 func (s *ClientSuite) TestDownloadTargetTooLong(c *C) {
 	client := s.updatedClient(c)
-	remoteFile := s.remote.targets["/foo.txt"]
+	remoteFile := s.remote.targets["foo.txt"]
 	remoteFile.buf = bytes.NewReader([]byte("foo-ooo"))
 	var dest testDestination
-	c.Assert(client.Download("/foo.txt", &dest), IsNil)
+	c.Assert(client.Download("foo.txt", &dest), IsNil)
 	c.Assert(remoteFile.bytesRead, Equals, 3)
 	c.Assert(dest.deleted, Equals, false)
 	c.Assert(dest.String(), Equals, "foo")
@@ -843,19 +843,19 @@ func (s *ClientSuite) TestDownloadTargetTooLong(c *C) {
 
 func (s *ClientSuite) TestDownloadTargetTooShort(c *C) {
 	client := s.updatedClient(c)
-	remoteFile := s.remote.targets["/foo.txt"]
+	remoteFile := s.remote.targets["foo.txt"]
 	remoteFile.buf = bytes.NewReader([]byte("fo"))
 	var dest testDestination
-	c.Assert(client.Download("/foo.txt", &dest), DeepEquals, ErrWrongSize{"/foo.txt", 2, 3})
+	c.Assert(client.Download("foo.txt", &dest), DeepEquals, ErrWrongSize{"foo.txt", 2, 3})
 	c.Assert(dest.deleted, Equals, true)
 }
 
 func (s *ClientSuite) TestDownloadTargetCorruptData(c *C) {
 	client := s.updatedClient(c)
-	remoteFile := s.remote.targets["/foo.txt"]
+	remoteFile := s.remote.targets["foo.txt"]
 	remoteFile.buf = bytes.NewReader([]byte("corrupt"))
 	var dest testDestination
-	assertWrongHash(c, client.Download("/foo.txt", &dest))
+	assertWrongHash(c, client.Download("foo.txt", &dest))
 	c.Assert(dest.deleted, Equals, true)
 }
 
@@ -863,7 +863,7 @@ func (s *ClientSuite) TestAvailableTargets(c *C) {
 	client := s.updatedClient(c)
 	files, err := client.Targets()
 	c.Assert(err, IsNil)
-	assertFiles(c, files, []string{"/foo.txt"})
+	assertFiles(c, files, []string{"foo.txt"})
 
 	s.addRemoteTarget(c, "bar.txt")
 	s.addRemoteTarget(c, "baz.txt")
@@ -871,7 +871,7 @@ func (s *ClientSuite) TestAvailableTargets(c *C) {
 	c.Assert(err, IsNil)
 	files, err = client.Targets()
 	c.Assert(err, IsNil)
-	assertFiles(c, files, []string{"/foo.txt", "/bar.txt", "/baz.txt"})
+	assertFiles(c, files, []string{"foo.txt", "bar.txt", "baz.txt"})
 }
 
 func (s *ClientSuite) TestAvailableTarget(c *C) {
@@ -879,11 +879,11 @@ func (s *ClientSuite) TestAvailableTarget(c *C) {
 
 	target, err := client.Target("foo.txt")
 	c.Assert(err, IsNil)
-	assertFile(c, target, "/foo.txt")
+	assertFile(c, target, "foo.txt")
 
 	target, err = client.Target("/foo.txt")
 	c.Assert(err, IsNil)
-	assertFile(c, target, "/foo.txt")
+	assertFile(c, target, "foo.txt")
 
 	_, err = client.Target("bar.txt")
 	c.Assert(err, Equals, ErrNotFound{"bar.txt"})
