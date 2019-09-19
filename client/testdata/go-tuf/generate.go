86511f9254ba1f5cf41ba07bf56a599cf51da9c7
Erick Tryzelaar <etryzelaar@google.com>
2019-09-19 09:14:43 -0700

    Add test to make sure go-tuf interop data doesn't change
    This is a test that makes sure that the metadata produced by go-tuf
    cannot change out from under it without this metadata from being
    regenerated. It does this by turning the generator script into a
    library, and then doing a diff between the snapshotted metadata
    and the one we just generated.
    
    Change-Id: I3449bc000f77be6495e3198a786693eef40e446c

--- client/testdata/go-tuf/generate.go
+++ client/testdata/go-tuf/generate.go
@@ -1,146 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"time"
 
-	tuf "github.com/flynn/go-tuf"
-	"github.com/flynn/go-tuf/sign"
+	"github.com/flynn/go-tuf/client/testdata/go-tuf/generator"
 )
-
-var expirationDate = time.Date(2100, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-type persistedKeys struct {
-	Encrypted bool               `json:"encrypted"`
-	Data      []*sign.PrivateKey `json:"data"`
-}
-
-func assertNotNil(err error) {
-	if err != nil {
-		panic(fmt.Sprintf("assertion failed: %s", err))
-	}
-}
-
-func copyRepo(src string, dst string) {
-	cmd := exec.Command("cp", "-r", src, dst)
-	assertNotNil(cmd.Run())
-}
-
-func newRepo(dir string) *tuf.Repo {
-	repo, err := tuf.NewRepoIndent(tuf.FileSystemStore(dir, nil), "", "\t")
-	assertNotNil(err)
-
-	return repo
-}
-
-func commit(dir string, repo *tuf.Repo) {
-	assertNotNil(repo.SnapshotWithExpires(tuf.CompressionTypeNone, expirationDate))
-	assertNotNil(repo.TimestampWithExpires(expirationDate))
-	assertNotNil(repo.Commit())
-
-	// Remove the keys directory to make sure we don't accidentally use a key.
-	assertNotNil(os.RemoveAll(filepath.Join(dir, "keys")))
-}
-
-func addKeys(repo *tuf.Repo, roleKeys map[string][]*sign.PrivateKey) {
-	for role, keys := range roleKeys {
-		for _, key := range keys {
-			assertNotNil(repo.AddPrivateKeyWithExpires(role, key, expirationDate))
-		}
-	}
-}
-
-func addTargets(repo *tuf.Repo, dir string, files map[string][]byte) {
-	paths := []string{}
-	for file, data := range files {
-		path := filepath.Join(dir, "staged", "targets", file)
-		assertNotNil(os.MkdirAll(filepath.Dir(path), 0755))
-		assertNotNil(ioutil.WriteFile(path, data, 0644))
-		paths = append(paths, file)
-	}
-	assertNotNil(repo.AddTargetsWithExpires(paths, nil, expirationDate))
-}
-
-func revokeKeys(repo *tuf.Repo, role string, keys []*sign.PrivateKey) {
-	for _, key := range keys {
-		assertNotNil(repo.RevokeKeyWithExpires(role, key.PublicData().IDs()[0], expirationDate))
-	}
-}
-
-func generateRepos(dir string, consistentSnapshot bool) {
-	f, err := os.Open("../keys.json")
-	assertNotNil(err)
-
-	var roleKeys map[string][][]*sign.PrivateKey
-	assertNotNil(json.NewDecoder(f).Decode(&roleKeys))
-
-	// Collect all the initial keys we'll use when creating repositories.
-	// We'll modify this to reflect rotated keys.
-	keys := map[string][]*sign.PrivateKey{
-		"root":      roleKeys["root"][0],
-		"targets":   roleKeys["targets"][0],
-		"snapshot":  roleKeys["snapshot"][0],
-		"timestamp": roleKeys["timestamp"][0],
-	}
-
-	// Create the initial repo.
-	dir0 := filepath.Join(dir, "0")
-	repo0 := newRepo(dir0)
-	repo0.Init(consistentSnapshot)
-	addKeys(repo0, keys)
-	addTargets(repo0, dir0, map[string][]byte{"0": []byte("0")})
-	commit(dir0, repo0)
-
-	// Rotate all the keys to make sure that works.
-	oldDir := dir0
-	i := 1
-	for _, role := range []string{"root", "targets", "snapshot", "timestamp"} {
-		// Setup the repo.
-		stepName := fmt.Sprintf("%d", i)
-		d := filepath.Join(dir, stepName)
-		copyRepo(oldDir, d)
-		repo := newRepo(d)
-		addKeys(repo, keys)
-
-		// Actually rotate the keys
-		revokeKeys(repo, role, roleKeys[role][0])
-		addKeys(repo, map[string][]*sign.PrivateKey{
-			role: roleKeys[role][1],
-		})
-		keys[role] = roleKeys[role][1]
-
-		// Add a target to make sure that works, then commit.
-		addTargets(repo, d, map[string][]byte{stepName: []byte(stepName)})
-		commit(d, repo)
-
-		i += 1
-		oldDir = d
-	}
-
-	// Add another target file to make sure the workflow worked.
-	stepName := fmt.Sprintf("%d", i)
-	d := filepath.Join(dir, stepName)
-	copyRepo(oldDir, d)
-	repo := newRepo(d)
-	addKeys(repo, keys)
-	addTargets(repo, d, map[string][]byte{stepName: []byte(stepName)})
-	commit(d, repo)
-}
 
 func main() {
 	cwd, err := os.Getwd()
-	assertNotNil(err)
-
-	for _, consistentSnapshot := range []bool{false, true} {
-		name := fmt.Sprintf("consistent-snapshot-%t", consistentSnapshot)
-		log.Printf("generating %s", name)
-		generateRepos(filepath.Join(cwd, name), consistentSnapshot)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	generator.Generate(filepath.Join(cwd, "consistent-snapshot-false"), "../keys.json", false)
+	generator.Generate(filepath.Join(cwd, "consistent-snapshot-true"), "../keys.json", true)
 }
