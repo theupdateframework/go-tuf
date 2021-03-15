dd6814fcb7344c058d25554852074acc54c1e49e
Laurent Meunier <laurmeun.business@gmail.com>
2021-03-15 16:41:53 +0100

    fix: chmod windows compatible

--- util/util.go
+++ util/util.go
@@ -280,7 +280,7 @@ func AtomicallyWriteFile(filename string, data []byte, perm os.FileMode) error {
 		return err
 	}
 
-	if err = f.Chmod(perm); err != nil {
+	if err = os.Chmod(filename, perm); err != nil {
 		f.Close()
 		os.Remove(f.Name())
 		return err
