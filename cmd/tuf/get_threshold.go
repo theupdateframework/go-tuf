7681f9686cad34a11fd2865efb4f477724b61408
Asra Ali <asraa@google.com>
2021-06-24 12:28:12 -0400

    add getter
    Signed-off-by: Asra Ali <asraa@google.com>

---
+++ cmd/tuf/get_threshold.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/flynn/go-docopt"
+	"github.com/theupdateframework/go-tuf"
+)
+
+func init() {
+	register("get-threshold", cmdGetThreshold, `
+usage: tuf get-threshold <role>
+
+Gets the threshold for a role.  
+`)
+}
+
+func cmdGetThreshold(args *docopt.Args, repo *tuf.Repo) error {
+	role := args.String["<role>"]
+
+	threshold, err := repo.GetThreshold(role)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Got", role, "threshold", threshold)
+	return nil
+}
