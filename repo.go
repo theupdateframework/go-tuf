6b6273e4e6e85a2d49ea88e44e0b5e42be2dbbf4
Asra Ali <asraa@google.com>
2021-06-25 10:52:37 -0400

    error condition when delegation
    Signed-off-by: Asra Ali <asraa@google.com>

--- repo.go
+++ repo.go
@@ -181,6 +181,11 @@ func (r *Repo) GetThreshold(keyRole string) (int, error) {
 }
 
 func (r *Repo) SetThreshold(keyRole string, t int) error {
+	if !validManifest(keyRole) {
+		// Delegations are not currently supported, so return an error if this is not a
+		// top-level manifest.
+		return ErrInvalidRole{keyRole}
+	}
 	root, err := r.root()
 	if err != nil {
 		return err
