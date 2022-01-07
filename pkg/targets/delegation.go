38b372908c49212125b87fc49a0053f80587b94e
Ethan Lowman <ethan.lowman@datadoghq.com>
2022-01-07 11:45:28 -0500

    stash

--- pkg/targets/delegation.go
+++ pkg/targets/delegation.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 
 	"github.com/theupdateframework/go-tuf/data"
-	"github.com/theupdateframework/go-tuf/internal/sets"
 	"github.com/theupdateframework/go-tuf/verify"
 )
 
@@ -25,11 +24,12 @@ var ErrTopLevelTargetsRoleMissing = errors.New("tuf: top level targets role miss
 // NewDelegationsIterator initialises an iterator with a first step
 // on top level targets.
 func NewDelegationsIterator(target string, topLevelKeysDB *verify.DB) *delegationsIterator {
-	role := topLevelKeysDB.GetRole("targets")
+	// role := topLevelKeysDB.GetRole("targets")
 	keyIDs := []string{}
-	if role != nil {
-		keyIDs = sets.StringSetToSlice(role.KeyIDs)
-	}
+
+	// if role != nil {
+	// 	keyIDs = sets.StringSetToSlice(role.KeyIDs)
+	// }
 
 	i := &delegationsIterator{
 		target: target,
