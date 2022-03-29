941f7eb926915e7d4efbeba9dc480faf7e65289e
Radoslav Dimitrov <dimitrovr@vmware.com>
2022-03-30 00:42:47 +0300

    chore: update unit test to expect a deleted targets folder
    Signed-off-by: Radoslav Dimitrov <dimitrovr@vmware.com>

--- repo_test.go
+++ repo_test.go
@@ -1477,7 +1477,7 @@ func (rs *RepoSuite) TestManageMultipleTargets(c *C) {
 	c.Assert(r.Snapshot(), IsNil)
 	c.Assert(r.Timestamp(), IsNil)
 	c.Assert(r.Commit(), IsNil)
-	tmp.assertEmpty("repository/targets")
+	tmp.assertNotExist("repository/targets")
 	t, err := r.topLevelTargets()
 	c.Assert(err, IsNil)
 	c.Assert(t.Targets, HasLen, 0)
