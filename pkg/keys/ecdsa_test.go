f1e59c9d88f3f99dc0af413d687af4a969369135
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2022-08-23 20:48:18 -0500

    New : Go 1.18 Fuzzing
    Co-authored-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
    Co-authored-by: nathannaveen <42319948+nathannaveen@users.noreply.github.com>
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
    Signed-off-by: nathannaveen <42319948+nathannaveen@users.noreply.github.com>

--- pkg/keys/ecdsa_test.go
+++ pkg/keys/ecdsa_test.go
@@ -7,17 +7,53 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
-	"io"
-	"strings"
-
 	fuzz "github.com/google/gofuzz"
 	"github.com/theupdateframework/go-tuf/data"
 	. "gopkg.in/check.v1"
+	"io"
+	"strings"
+	"testing"
 )
 
 type ECDSASuite struct{}
 
 var _ = Suite(&ECDSASuite{})
+
+// keySize for ECDSA equals 40 because, 256 / 8 = 32, and 32 + 8 = 40
+
+const keySize = 40
+
+func FuzzUnmarshalECDSA(f *testing.F) {
+	f.Add("00001-deterministic-buffer-for-key-generation")
+
+	f.Fuzz(func(t *testing.T, s string) {
+		if len(s) <= keySize {
+			// len(s) <= 40 is because 256 / 8 = 32, and 32 + 8 = 40, this is from the bitsize of the curve
+			t.Skip()
+		}
+
+		c := &C{}
+		priv, err := ecdsa.GenerateKey(elliptic.P256(), strings.NewReader(s))
+		c.Assert(err, IsNil)
+
+		// Marshall as non compressed point
+		pub := elliptic.Marshal(elliptic.P256(), priv.X, priv.Y)
+
+		publicKey, err := json.Marshal(map[string]string{
+			"public": hex.EncodeToString(pub),
+		})
+		c.Assert(err, IsNil)
+
+		badKey := &data.PublicKey{
+			Type:       data.KeyTypeECDSA_SHA2_P256,
+			Scheme:     data.KeySchemeECDSA_SHA2_P256,
+			Algorithms: data.HashAlgorithms,
+			Value:      publicKey,
+		}
+		verifier := NewEcdsaVerifier()
+		c.Assert(verifier.UnmarshalPublicKey(badKey), IsNil)
+	})
+}
 
 func (ECDSASuite) TestUnmarshalECDSA(c *C) {
 	priv, err := ecdsa.GenerateKey(elliptic.P256(), strings.NewReader("00001-deterministic-buffer-for-key-generation"))
@@ -39,6 +75,32 @@ func (ECDSASuite) TestUnmarshalECDSA(c *C) {
 	}
 	verifier := NewEcdsaVerifier()
 	c.Assert(verifier.UnmarshalPublicKey(badKey), IsNil)
+}
+
+func FuzzInvalidUnmarshalECDSA(f *testing.F) {
+	f.Fuzz(func(t *testing.T, s string) {
+		if len(s) > keySize {
+			// len(s) > 40 is because 256 / 8 = 32, and 32 + 8 = 40, this is from the bitsize of the curve, we skip
+			// this because only then will we get bad data
+			t.Skip()
+		}
+
+		badKeyValue, err := json.Marshal(s)
+		if err != nil {
+			t.Skip()
+		}
+
+		c := &C{}
+
+		badKey := &data.PublicKey{
+			Type:       data.KeyTypeECDSA_SHA2_P256,
+			Scheme:     data.KeySchemeECDSA_SHA2_P256,
+			Algorithms: data.HashAlgorithms,
+			Value:      badKeyValue,
+		}
+		verifier := NewEcdsaVerifier()
+		c.Assert(verifier.UnmarshalPublicKey(badKey), ErrorMatches, "json: cannot unmarshal.*")
+	})
 }
 
 func (ECDSASuite) TestUnmarshalECDSA_Invalid(c *C) {
