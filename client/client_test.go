39a673beeb8da9e8012a714184ac4cf4a6922824
Cédric Van Rompay <cedric.vanrompay@datadoghq.com>
2022-09-02 20:07:18 +0200

    add some comments

--- client/client_test.go
+++ client/client_test.go
@@ -1245,6 +1245,28 @@ type StateLessSuite struct{}
 var _ = Suite(&StateLessSuite{})
 
 func (s *StateLessSuite) TestRejectsMultiSignaturesSameKeyDifferentIDs(c *C) {
+	// In this test Alice and Bob want to create a TUF repo
+	// where a root key rotation would require both their signatures.
+	// Alice uses an old version of Go-TUF where each key gets assigned several IDs.
+	// Bob uses a modern version of Go-TUF that does not produce the same list of IDs for a same key.
+	// This test checks that the TUF client
+	// will not accept a root rotation
+	// signed twice with Alice's key with different key IDs each time.
+	// This test was failing with https://github.com/theupdateframework/go-tuf/tree/ac7b5d7bce18cca5a84a28b021bd6372f450b35b
+	// because the signature verification code was assuming that the key IDs used in the metadata
+	// were the same as the one the TUF library of the client would generate,
+	// breaking the security of threshold signatures.
+
+	// The attack works just the same if Alice is malicious from the beginning
+	// and convinces Bob to sign an initial "root.json"
+	// with additional key IDs for her only key,
+	// but this scenario show that the vulnerability can even impact situations
+	// where Alice is not malicious at all,
+	// she was simply using an old client and an attacker stole her key.
+	// The purpose of threshold signatures in TUF is precisely
+	// to make sure that an attacker cannot forge signatures
+	// if they did not steal a large enough number of keys.
+
 	alice, err := keys.GenerateEd25519Key()
 	if err != nil {
 		panic(err)
@@ -1254,7 +1276,7 @@ func (s *StateLessSuite) TestRejectsMultiSignaturesSameKeyDifferentIDs(c *C) {
 	root.Version = 1
 	root.Roles["root"] = &data.Role{
 		KeyIDs:    []string{},
-		Threshold: 2,
+		Threshold: 2, // Note the threshold
 	}
 
 	// reproduces how IDs were computed in
@@ -1264,10 +1286,6 @@ func (s *StateLessSuite) TestRejectsMultiSignaturesSameKeyDifferentIDs(c *C) {
 		digest := sha256.Sum256(bytes)
 		ids := []string{hex.EncodeToString(digest[:])}
 
-		// FIXME(TUF-0.9) If we receive TUF-1.0 compatible metadata,
-		// the key id we just calculated won't be compatible with
-		// TUF-0.9. So we also need to calculate the TUF-0.9 key id to
-		// be backwards compatible.
 		if k.Scheme != "" || len(k.Algorithms) != 0 {
 			bytes, _ = cjson.EncodeCanonical(&data.PublicKey{
 				Type:  k.Type,
@@ -1298,6 +1316,7 @@ func (s *StateLessSuite) TestRejectsMultiSignaturesSameKeyDifferentIDs(c *C) {
 		bob.PublicData().IDs()...,
 	)
 
+	// signer for the other roles, not important
 	delegatedSigner, _ := keys.GenerateEd25519Key()
 	root.AddKey(delegatedSigner.PublicData())
 	for _, role := range []string{"targets", "snapshot", "timestamp"} {
@@ -1335,7 +1354,7 @@ func (s *StateLessSuite) TestRejectsMultiSignaturesSameKeyDifferentIDs(c *C) {
 	c.Assert(err, IsNil)
 
 	// checking that client does not accept root rotation
-	// to evil root (was only signed with Alice's key)
+	// to evil root
 
 	localStore := MemoryLocalStore()
 	err = localStore.SetMeta("root.json", rootJSON)
