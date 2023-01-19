20ebbef51a013ca37088daf9686bf5191a813e1d
Radoslav Dimitrov <dimitrovr@vmware.com>
2023-01-19 14:50:40 +0200

    chore: add tests for metadata default values
    Signed-off-by: Radoslav Dimitrov <dimitrovr@vmware.com>

--- metadata/metadata_test.go
+++ metadata/metadata_test.go
@@ -20,35 +20,121 @@ import (
 
 func TestRootDefaultValues(t *testing.T) {
 	// without setting expiration
-	root := Root()
-	assert.NotNil(t, root)
-	assert.GreaterOrEqual(t, []time.Time{time.Now().UTC()}[0], root.Signed.Expires)
+	meta := Root()
+	assert.NotNil(t, meta)
+	assert.GreaterOrEqual(t, []time.Time{time.Now().UTC()}[0], meta.Signed.Expires)
 
 	// setting expiration
 	expire := time.Now().AddDate(0, 0, 2).UTC()
-	root = Root(expire)
-	assert.NotNil(t, root)
-	assert.Equal(t, expire, root.Signed.Expires)
+	meta = Root(expire)
+	assert.NotNil(t, meta)
+	assert.Equal(t, expire, meta.Signed.Expires)
 
 	// Type
-	assert.Equal(t, ROOT, root.Signed.Type)
+	assert.Equal(t, ROOT, meta.Signed.Type)
 
 	// SpecVersion
-	assert.Equal(t, SPECIFICATION_VERSION, root.Signed.SpecVersion)
+	assert.Equal(t, SPECIFICATION_VERSION, meta.Signed.SpecVersion)
 
 	// Version
-	assert.Equal(t, int64(1), root.Signed.Version)
+	assert.Equal(t, int64(1), meta.Signed.Version)
 
 	// Threshold and KeyIDs for Roles
 	for _, role := range []string{ROOT, SNAPSHOT, TARGETS, TIMESTAMP} {
-		assert.Equal(t, 1, root.Signed.Roles[role].Threshold)
-		assert.Equal(t, []string{}, root.Signed.Roles[role].KeyIDs)
+		assert.Equal(t, 1, meta.Signed.Roles[role].Threshold)
+		assert.Equal(t, []string{}, meta.Signed.Roles[role].KeyIDs)
 	}
 
 	// Keys
-	assert.Equal(t, map[string]*Key{}, root.Signed.Keys)
+	assert.Equal(t, map[string]*Key{}, meta.Signed.Keys)
 
 	// Consistent snapshot
-	assert.True(t, root.Signed.ConsistentSnapshot)
+	assert.True(t, meta.Signed.ConsistentSnapshot)
 
+	// Signatures
+	assert.Equal(t, []Signature{}, meta.Signatures)
+}
+
+func TestSnapshotDefaultValues(t *testing.T) {
+	// without setting expiration
+	meta := Snapshot()
+	assert.NotNil(t, meta)
+	assert.GreaterOrEqual(t, []time.Time{time.Now().UTC()}[0], meta.Signed.Expires)
+
+	// setting expiration
+	expire := time.Now().AddDate(0, 0, 2).UTC()
+	meta = Snapshot(expire)
+	assert.NotNil(t, meta)
+	assert.Equal(t, expire, meta.Signed.Expires)
+
+	// Type
+	assert.Equal(t, SNAPSHOT, meta.Signed.Type)
+
+	// SpecVersion
+	assert.Equal(t, SPECIFICATION_VERSION, meta.Signed.SpecVersion)
+
+	// Version
+	assert.Equal(t, int64(1), meta.Signed.Version)
+
+	// Targets meta
+	assert.Equal(t, map[string]MetaFiles{"targets.json": {Version: 1}}, meta.Signed.Meta)
+
+	// Signatures
+	assert.Equal(t, []Signature{}, meta.Signatures)
+}
+
+func TestTimestampDefaultValues(t *testing.T) {
+	// without setting expiration
+	meta := Timestamp()
+	assert.NotNil(t, meta)
+	assert.GreaterOrEqual(t, []time.Time{time.Now().UTC()}[0], meta.Signed.Expires)
+
+	// setting expiration
+	expire := time.Now().AddDate(0, 0, 2).UTC()
+	meta = Timestamp(expire)
+	assert.NotNil(t, meta)
+	assert.Equal(t, expire, meta.Signed.Expires)
+
+	// Type
+	assert.Equal(t, TIMESTAMP, meta.Signed.Type)
+
+	// SpecVersion
+	assert.Equal(t, SPECIFICATION_VERSION, meta.Signed.SpecVersion)
+
+	// Version
+	assert.Equal(t, int64(1), meta.Signed.Version)
+
+	// Snapshot meta
+	assert.Equal(t, map[string]MetaFiles{"snapshot.json": {Version: 1}}, meta.Signed.Meta)
+
+	// Signatures
+	assert.Equal(t, []Signature{}, meta.Signatures)
+}
+
+func TestTargetsDefaultValues(t *testing.T) {
+	// without setting expiration
+	meta := Targets()
+	assert.NotNil(t, meta)
+	assert.GreaterOrEqual(t, []time.Time{time.Now().UTC()}[0], meta.Signed.Expires)
+
+	// setting expiration
+	expire := time.Now().AddDate(0, 0, 2).UTC()
+	meta = Targets(expire)
+	assert.NotNil(t, meta)
+	assert.Equal(t, expire, meta.Signed.Expires)
+
+	// Type
+	assert.Equal(t, TARGETS, meta.Signed.Type)
+
+	// SpecVersion
+	assert.Equal(t, SPECIFICATION_VERSION, meta.Signed.SpecVersion)
+
+	// Version
+	assert.Equal(t, int64(1), meta.Signed.Version)
+
+	// Target files
+	assert.Equal(t, map[string]TargetFiles{}, meta.Signed.Targets)
+
+	// Signatures
+	assert.Equal(t, []Signature{}, meta.Signatures)
 }
