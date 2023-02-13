15e20370dd9ef9d2a73353069e5ffd2a5c967b3f
Radoslav Dimitrov <dimitrovr@vmware.com>
2023-02-13 16:20:39 +0200

    fix: change default value for hash prefixed targets
    Signed-off-by: Radoslav Dimitrov <dimitrovr@vmware.com>

--- metadata/config/config.go
+++ metadata/config/config.go
@@ -31,6 +31,6 @@ func New() *UpdaterConfig {
 		TimestampMaxLength:    16384,   // bytes
 		SnapshotMaxLength:     2000000, // bytes
 		TargetsMaxLength:      5000000, // bytes
-		PrefixTargetsWithHash: true,
+		PrefixTargetsWithHash: false,
 	}
 }
