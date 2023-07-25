8dd4c7574b34595b90451580022cd1d8333ad45f
Ivana Atanasova <iyovcheva@vmware.com>
2023-07-25 12:41:42 +0300

    test: add metadata unit tests (#45)
    * Add test repository data
    
    This change adds repository data to use in unit testing, keeping
    it the same as in python-tuf
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    * Add file system simulator
    
    This change adds a mock for os functions
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    * Add test client simulator
    
    This change implements client simulator for working with local
    repositories
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    * Add metadata unit tests
    
    This change adds unit tests for part of the metadata methods
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    * Add metadata api tests
    
    This change implements api unit tests for metadata with the same
    logic as in python-tuf
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    * Fix go linter in test simulators
    
    This change fixes go linter error of not checking errors returned
    by methods in the file system and test client simulators
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    * Create clean test metadata repository
    
    As the `signature` package allows loading keys from RSA PEM files
    only, the keys and roles from the python-tuf implementation are
    not suitable for the current implementation. This is why a clean
    repo is created with new keys in the appropriate format
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    * Add metadata sign/verify test
    
    This change adds test coverage for signing and verigying metadata
    with a pre-set keys
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    * Cleanup unnecessary functionality
    
    This change removes unused keys and test utils for the metadata
    unit tests. The removed test utils include filesystem simulator,
    which is replaced by the setup functionality and some tidy-up
    around the changes
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    * Fix metadata api tests to verify error type
    
    This change updates error assertions in the metadata api tests
    to compare error types rather than only a message
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    * Improve metadata tests
    
    This change provides improvements to the metadata api tests
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    
    ---------
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>
    Co-authored-by: Ivana Atanasova <iyovcheva@iyovcheva7MD6R.vmware.com>

---
+++ testutils/testutils/setup.go
@@ -0,0 +1,96 @@
+// Copyright 2023 VMware, Inc.
+//
+// This product is licensed to you under the BSD-2 license (the "License").
+// You may not use this product except in compliance with the BSD-2 License.
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to
+// the terms and conditions of the subcomponent's license, as noted in the
+// LICENSE file.
+//
+// SPDX-License-Identifier: BSD-2-Clause
+
+package testutils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	TempDir     string
+	RepoDir     string
+	KeystoreDir string
+)
+
+func SetupTestDirs() error {
+	tmp := os.TempDir()
+	var err error
+	TempDir, err = os.MkdirTemp(tmp, "0750")
+	if err != nil {
+		log.Fatal("failed to create temporary directory: ", err)
+		return err
+	}
+
+	RepoDir = fmt.Sprintf("%s/repository_data/repository", TempDir)
+	absPath, err := filepath.Abs("../testutils/repository_data/repository/metadata")
+	if err != nil {
+		log.Debugf("failed to get absolute path: %v", err)
+	}
+	err = Copy(absPath, RepoDir)
+	if err != nil {
+		log.Debugf("failed to copy metadata to %s: %v", RepoDir, err)
+		return err
+	}
+
+	KeystoreDir = fmt.Sprintf("%s/keystore", TempDir)
+	err = os.Mkdir(KeystoreDir, 0750)
+	if err != nil {
+		log.Debugf("failed to create keystore dir %s: %v", KeystoreDir, err)
+	}
+	absPath, err = filepath.Abs("../testutils/repository_data/keystore")
+	if err != nil {
+		log.Debugf("failed to get absolute path: %v", err)
+	}
+	err = Copy(absPath, KeystoreDir)
+	if err != nil {
+		log.Debugf("failed to copy keystore to %s: %v", KeystoreDir, err)
+		return err
+	}
+
+	return nil
+}
+
+func Copy(fromPath string, toPath string) error {
+	err := os.MkdirAll(toPath, 0750)
+	if err != nil {
+		log.Debugf("failed to create directory %s: %v", toPath, err)
+	}
+	files, err := os.ReadDir(fromPath)
+	if err != nil {
+		log.Debugf("failed to read path %s: %v", fromPath, err)
+		return err
+	}
+	for _, file := range files {
+		data, err := os.ReadFile(fmt.Sprintf("%s/%s", fromPath, file.Name()))
+		if err != nil {
+			log.Debugf("failed to read file %s: %v", file.Name(), err)
+		}
+		filePath := fmt.Sprintf("%s/%s", toPath, file.Name())
+		err = os.WriteFile(filePath, data, 0750)
+		if err != nil {
+			log.Debugf("failed to write file %s: %v", filePath, err)
+		}
+	}
+	return nil
+}
+
+func Cleanup() {
+	log.Printf("cleaning temporary directory: %s\n", TempDir)
+	err := os.RemoveAll(TempDir)
+	if err != nil {
+		log.Fatalf("failed to cleanup test directories: %v", err)
+	}
+}
