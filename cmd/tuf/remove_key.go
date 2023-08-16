61b52ebcd4564675020c4840413eb3bfcf0a2beb
JuliannaReyes <116505933+JuliannaReyes@users.noreply.github.com>
2023-08-16 18:17:02 +0000

    fixed expiration value of remove_key command
    Signed-off-by: JuliannaReyes <116505933+JuliannaReyes@users.noreply.github.com>

--- cmd/tuf/remove_key.go
+++ cmd/tuf/remove_key.go
@@ -17,15 +17,36 @@ Remove a signing key
 Before the key is removed the key will be first revoked
 The key will then be removed from the root metadata file and if the key is present in 
 "keys" directory it will also be removed
+
+Options:
+  --expires=<days>   Set the root metadata file to expire <days> days from now.
 `)
 }
 
 func cmdRemoveKey(args *docopt.Args, repo *tuf.Repo) error {
 	role := args.String["<roles>"]
 	keyID := args.String["<id>"]
+
+	// Extract the --ecpires option value
+	expires, _ := args.Int("--expires")
+
+	// Revoke the key 
 	if err := repo.RevokeKey(role, keyID); err != nil {
 		return err
 	}
+
+	// Update expiration vaalue if --expires is provided 
+	if expires > 0 {
+		rooMetadata := repo.Root()
+		if rootMetadata != nil {
+			newExpires := time.Now().Add(time.Duration(expires) * 24 * time.Hour)
+			rootMetadata.Expires = &newExpires 
+			if err := repo.Commit(); err != nil {
+				return err
+			}
+		}
+	}
+
 	keyPath := filepath.Join("keys", keyID)
 	if _, err := os.Stat(keyPath); err == nil {
 		if err := os.Remove(keyPath); err != nil {
