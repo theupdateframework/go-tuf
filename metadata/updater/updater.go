69355c68de1fe437dc77de3dc0230fe4fa873cb3
udf2457 <udf2457@users.noreply.github.com>
2025-12-09 15:56:55 +0000

    Improve error handling for temporary file removal
    Join error messages when failing to remove temporary files.
    
    Signed-off-by: udf2457 <udf2457@users.noreply.github.com>

--- metadata/updater/updater.go
+++ metadata/updater/updater.go
@@ -605,7 +605,7 @@ func (update *Updater) persistMetadata(roleName string, data []byte) error {
 		if errRemove != nil {
 			log.Info("Failed to delete temporary file", "name", file.Name())
 		}
-		return err
+		return errors.Join(err, errRemove)
 	}
 	// write the data content to the temporary file
 	_, err = file.Write(data)
@@ -616,7 +616,7 @@ func (update *Updater) persistMetadata(roleName string, data []byte) error {
 		if errRemove != nil {
 			log.Info("Failed to delete temporary file", "name", file.Name())
 		}
-		return err
+		return errors.Join(err, errRemove)
 	}
 
 	// can't move/rename an open file on windows, so close it first
