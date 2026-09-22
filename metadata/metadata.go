theupdateframework/go-tuf

commit ea170bfe8108df2310db79c39fdee42186d21e01

Metadata tests followup (#54)
* Fix AddKey for targets

Previously Targets' `AddKey(key, role)` was returning error that
role doesn't exist in the case when role exists and already has
that key. This change fixes that and alligns the function with the
python-tuf reference implementation, where the key is added to
delegations, even if it's not added to Roles and SuccinctRoles

Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

* Fix delegated path prefix check

Previously the target file path hash was not base64 URL encoded
which ended up to not being able to properly compare if role's
PathHashPrefixes contains a prefix of that hash. This change
fixes the encoding

Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

* Add SuccinctRoles.GetSuffixLen() for the purpose of testing

This allows testing if the suffix len is properly calculated in
the metadata

Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

* Add test repository data improvements

This change changes the targets files used for targets metadata
to be simplified and neutral for the purpose of more clear and less
mistake-prone test cases. It also adds new keys for additional
metadata api testing

Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

* Add more metadata api tests

This change completes the metadata api test coverage with
reference to the python-tuf implementations, excluding few use-
cases that are not applicable

Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

* Fix go linter checks for metadata api tests

This fixes few error checks that were not handled in the metadata
api tests

Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

* Improve metadata hepler test functions usage

This change provides tiny updates of two metalada helper functions
to giarantee more stable testing over time

Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

---------

Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

metadata/metadata.go
@@ -17,6 +17,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"crypto/sha512"
+	"encoding/base64"
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
@@ -514,7 +515,7 @@ func (role *DelegatedRole) IsDelegatedPath(targetFilepath string) (bool, error) 
 		// hash bin delegations - calculate the hash of the filepath to determine in which bin to find the target.
 		targetFilepathHash := sha256.Sum256([]byte(targetFilepath))
 		for _, pathHashPrefix := range role.PathHashPrefixes {
-			if strings.HasPrefix(string(targetFilepathHash[:]), pathHashPrefix) {
+			if strings.HasPrefix(base64.URLEncoding.EncodeToString(targetFilepathHash[:]), pathHashPrefix) {
 				return true, nil
 			}
 		}
@@ -573,8 +574,7 @@ func (role *SuccinctRoles) GetRolesForTarget(targetFilepath string) map[string]b
 // GetRoles returns the names of all different delegated roles
 func (role *SuccinctRoles) GetRoles() []string {
 	res := []string{}
-	numberOfBins := int(math.Pow(2, float64(role.BitLength)))
-	suffixLen := len(strconv.FormatInt(int64(numberOfBins-1), 16))
+	suffixLen, numberOfBins := role.GetSuffixLen()
 
 	for binNumber := 0; binNumber < numberOfBins; binNumber++ {
 		suffix := fmt.Sprintf("%0*x", suffixLen, binNumber)
@@ -583,11 +583,15 @@ func (role *SuccinctRoles) GetRoles() []string {
 	return res
 }
 
+func (role *SuccinctRoles) GetSuffixLen() (int, int) {
+	numberOfBins := int(math.Pow(2, float64(role.BitLength)))
+	return len(strconv.FormatInt(int64(numberOfBins-1), 16)), numberOfBins
+}
+
 // IsDelegatedRole returns whether the given roleName is in one of
 // the delegated roles that “SuccinctRoles“ represents
 func (role *SuccinctRoles) IsDelegatedRole(roleName string) bool {
-	numberOfBins := int64(math.Pow(2, float64(role.BitLength)))
-	suffixLen := len(strconv.FormatInt(int64(numberOfBins-1), 16))
+	suffixLen, numberOfBins := role.GetSuffixLen()
 
 	expectedPrefix := fmt.Sprintf("%s-", role.NamePrefix)
 
@@ -609,7 +613,7 @@ func (role *SuccinctRoles) IsDelegatedRole(roleName string) bool {
 	}
 
 	// check if the bin we calculated is indeed within the range of what we support
-	return (value >= 0) && (value < numberOfBins)
+	return (value >= 0) && (value < int64(numberOfBins))
 }
 
 // AddKey adds new signing key for delegated role "role"
@@ -674,9 +678,11 @@ func (signed *TargetsType) AddKey(key *Key, role string) error {
 	// standard delegated roles
 	if signed.Delegations.Roles != nil {
 		// loop through all delegated roles
+		isDelegatedRole := false
 		for i, d := range signed.Delegations.Roles {
 			// if role is found
 			if d.Name == role {
+				isDelegatedRole = true
 				// add key if keyID is not already part of keyIDs for that role
 				if !slices.Contains(d.KeyIDs, key.ID()) {
 					signed.Delegations.Roles[i].KeyIDs = append(signed.Delegations.Roles[i].KeyIDs, key.ID())
@@ -685,6 +691,9 @@ func (signed *TargetsType) AddKey(key *Key, role string) error {
 				}
 				log.Debugf("Delegated role %s already has keyID %s", role, key.ID())
 			}
+		}
+		if !isDelegatedRole {
+			return ErrValue{Msg: fmt.Sprintf("delegated role %s doesn't exist", role)}
 		}
 	} else if signed.Delegations.SuccinctRoles != nil {
 		// add key if keyID is not already part of keyIDs for the SuccinctRoles role
@@ -696,7 +705,8 @@ func (signed *TargetsType) AddKey(key *Key, role string) error {
 		log.Debugf("SuccinctRoles role already has keyID %s", key.ID())
 
 	}
-	return ErrValue{Msg: fmt.Sprintf("delegated role %s doesn't exist", role)}
+	signed.Delegations.Keys[key.ID()] = key // TODO: should we check if we don't accidentally override an existing keyID with another key value?
+	return nil
 }
 
 // RevokeKey revokes key from delegated role "role" and updates the delegations key store

metadata/metadata_test.go
@@ -29,13 +29,13 @@ const TEST_REPOSITORY_DATA = "../testutils/repository_data/repository/metadata"
 
 var fixedExpire = time.Date(2030, 8, 15, 14, 30, 45, 100, time.UTC)
 
-func getSignatureByKeyID(signatures []Signature, keyID string) HexBytes {
-	for _, sig := range signatures {
+func getSignatureByKeyID(signatures []Signature, keyID string) (HexBytes, int) {
+	for i, sig := range signatures {
 		if sig.KeyID == keyID {
-			return sig.Signature
+			return sig.Signature, i
 		}
 	}
-	return []byte{}
+	return []byte{}, 0
 }
 
 func TestDefaultValuesRoot(t *testing.T) {

testutils/repository_data/keystore/root_key2
@@ -1,0 +1,15 @@
+-----BEGIN RSA PRIVATE KEY-----
+MIICXwIBAAKBgQC9sG1iNSW+JbnSuITAuWL46YphgqOXU3DhGZp2hM/1FaaRzLlR
+HTyH7EvzbKqlaGBs9Caatx6KlN7e8J45vpLO1PkKnLOHdKBuy39Fzxw1dhxJWvTI
+0mNDv68JbWxvgRxjEPE7wc24jYubovxiiBD8g2DbOnWEkyt7owECPRlXAwIDAQAB
+AoGBAKquh54oqG9yTsRXF8y6g13p9oRLIpxVjmpduWkPlHe5JYpnphBguEitzKGa
+k+oGA03GWr44K5kS33/HDvhyjHFXCqniittlVUwbaa4kbJiWi4lQ3K/m8F2DzHJP
+s4YaqmzG30v9j9z3nOgLhM7iye65beala72zJnGOXivUAuhRAkEA3KnfY/SFjTbo
+rsluVa03hC5KVQm/yDf4wShDnl8drYHnJ1XFkSC0UbBruRyu8JeWE93dAKu9IxdK
+WEdHOtxR3wJBANwQwX/wPJ8/+yo4lToyUuN0omx94sK/JuRmvi9dzCbdfQbkgvDa
+MEyWc0LNwxDBPYJ2bej/ORGmD+nOJo59h10CQQCCj/x+jvrKcFfCu6qOBRyZGC6h
+HFCebgfAektwFIVh2T/lNUndsgUfZIyIjeEwt/Bzts2CDRuu/KPfkeUifaPvAkEA
+m9iR8FTl2bGp4cCojcpNwR88V7DfAiP1GxNX5Jt8lJmOjW8O/BrI0bRKdCjb1+XB
+9b6BH9x/QexkoKOJ0qc7UQJBAINLHep9QG2b3AEGZ692Z+iyU1Lub7rWNBKsmodh
+0x9rwYs0D0EJo0BYozYhExz7ugaSzXW61H26IWbHtsg+5a0=
+-----END RSA PRIVATE KEY-----

testutils/repository_data/keystore/root_key2.pub
@@ -1,0 +1,6 @@
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9sG1iNSW+JbnSuITAuWL46Yph
+gqOXU3DhGZp2hM/1FaaRzLlRHTyH7EvzbKqlaGBs9Caatx6KlN7e8J45vpLO1PkK
+nLOHdKBuy39Fzxw1dhxJWvTI0mNDv68JbWxvgRxjEPE7wc24jYubovxiiBD8g2Db
+OnWEkyt7owECPRlXAwIDAQAB
+-----END PUBLIC KEY-----

testutils/testutils/setup.go
@@ -22,6 +22,7 @@ import (
 var (
 	TempDir     string
 	RepoDir     string
+	TargetsDir  string
 	KeystoreDir string
 )
 
@@ -40,6 +41,17 @@ func SetupTestDirs() error {
 		log.Debugf("failed to get absolute path: %v", err)
 	}
 	err = Copy(absPath, RepoDir)
+	if err != nil {
+		log.Debugf("failed to copy metadata to %s: %v", RepoDir, err)
+		return err
+	}
+
+	TargetsDir = fmt.Sprintf("%s/repository_data/repository/targets", TempDir)
+	targetsPath, err := filepath.Abs("../testutils/repository_data/repository/targets")
+	if err != nil {
+		log.Debugf("failed to get absolute targets path: %v", err)
+	}
+	err = Copy(targetsPath, TargetsDir)
 	if err != nil {
 		log.Debugf("failed to copy metadata to %s: %v", RepoDir, err)
 		return err

commit e32920e95b6285d49ff686c5d076f9f8af9a8ea4

metadata: correctly match path_hash_prefixes delegations
The specification clearly states that "path_hash_prefixes" are meant to
be interpreted as prefixes of the *hexadecimal representation* of the
SHA-256 hash of the target path.

However, go-tuf/v2 has never handled this correctly. Commit e0d9e3e2d67d
("feat: add support for succinct roles and hashbin delegations")
incorrectly tried to match against the raw byte values while commit
ea170bfe8108 ("Metadata tests followup (#54)") switched to base64
instead of hexadecimal (base16).

In both cases this meant that go-tuf/v2 would be unable to fetch the
target file that the repository publisher intended if they used
"path_hash_prefixes" (in most cases you would get a 404, in principle you
could have the wrong delegation provide the target). In practice, this
is not really possible for a malicious actor to exploit as SHA-256
hashes are (by design) unpredictable and "path_hash_prefixes" is
primarily intended as a fan-out mechanism rather than a trust boundary,
and so this is more a correctness fix than a security fix.

Fixes: ea170bfe8108 ("Metadata tests followup (#54)")
Fixes: e0d9e3e2d67d ("feat: add support for succinct roles and hashbin delegations")
Signed-off-by: Aleksa Sarai <aleksa@amutable.com>

metadata/metadata.go
@@ -25,7 +25,6 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"crypto/x509"
-	"encoding/base64"
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
@@ -581,7 +580,7 @@ func (role *DelegatedRole) IsDelegatedPath(targetFilepath string) (bool, error) 
 		// hash bin delegations - calculate the hash of the filepath to determine in which bin to find the target.
 		targetFilepathHash := sha256.Sum256([]byte(targetFilepath))
 		for _, pathHashPrefix := range role.PathHashPrefixes {
-			if strings.HasPrefix(base64.URLEncoding.EncodeToString(targetFilepathHash[:]), pathHashPrefix) {
+			if strings.HasPrefix(hex.EncodeToString(targetFilepathHash[:]), pathHashPrefix) {
 				return true, nil
 			}
 		}

metadata/metadata_api_test.go
@@ -1009,7 +1009,7 @@ func TestIsDelegatedRole(t *testing.T) {
 		KeyIDs:           []string{},
 		Threshold:        1,
 		Terminating:      false,
-		PathHashPrefixes: []string{"knsOz5xYT", "other prefix", "knsOz5xYT", "knsOz", "kn"},
+		PathHashPrefixes: []string{"927b0ecf9", "other prefix", "927b0ecf9", "927b0", "92"},
 	}
 	nonMatching, err = role.IsDelegatedPath("a/non-matching-path")
 	assert.NoError(t, err)